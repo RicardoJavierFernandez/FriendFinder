5a98a8335fb495e082bc046f9e21a2034f618ab5
RicardoJavierFernandez <ricky@Ricardos-MacBook-Pro.local>
2019-09-15 15:29:30 -0400

    Edited css and html of survey.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,9 @@
 var friendData = require('../data/friends');
 
 module.exports = function(app) {
-    app.get('api/friends', function(req, res) {
-        res.json(friendData)
+    app.get('/api/friends', function(req, res) {
+        res.json(friendData);
     });
-
     // app.post('api/friends', function(req, res) {
 
     // })
